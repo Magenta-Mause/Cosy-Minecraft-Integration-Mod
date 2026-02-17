970d4c4f377fe82948fc8558bd097f926ef9acc1
Janne Keipert <jabbekeipert@gmail.com>
2026-02-17 01:06:07 +0100

    Initialize mod project with Fabric API framework. Configure build scripts, Gradle wrapper, and base project structure. Add mixin definitions, data generator, and `fabric.mod.json`.

---
+++ src/client/java/com/magentamause/cosyintegrationmod/client/CosyintegrationmodDataGenerator.java
@@ -0,0 +1,12 @@
+package com.magentamause.cosyintegrationmod.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CosyintegrationmodDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
